ff67dd52facf2db1fff08f982351c6a9a8a8267a
Het Bhimani <hetbhimani2518@gmail.com>
2026-06-14 17:17:01 +0530

    Update CORS for Vercel deployment

--- src/main/java/com/example/hrms/config/SecurityConfig.java
+++ src/main/java/com/example/hrms/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "https://hrms-frontend-nine-psi.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
